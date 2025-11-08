c7fa8bfe7946e9678b8601c0d94abc2ca5c8da8d
abrilsar03 <abril.sar03@gmail.com>
2025-11-07 21:06:35 -0400

    feat(api): :sparkles: add user and role table definition

---
+++ src/main/resources/database/seed/migration/migration/V1__users_and_roles.sql
@@ -0,0 +1,31 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE roles (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  code TEXT NOT NULL UNIQUE, CHECK (code IN ('ADMIN', 'USER')),
+  name TEXT NOT NULL
+);
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  first_name TEXT NOT NULL,
+  last_name  TEXT NOT NULL,
+  email      TEXT NOT NULL UNIQUE,
+  phone_code TEXT,
+  phone      TEXT,
+  dni        TEXT,
+  dni_type   TEXT CHECK (dni_type IN ('V','E','J','DNI','CUIT','SSN')),
+  active     BOOLEAN NOT NULL DEFAULT TRUE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ
+);
+
+
+CREATE TABLE user_roles (
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  role_id UUID NOT NULL REFERENCES roles(id),
+  PRIMARY KEY (user_id, role_id)
+);
+
+
+CREATE INDEX idx_users_created_at ON users(created_at DESC);
