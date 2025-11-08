486ad755a315d89de2906d5c7eed2fcc8d04d834
abrilsar03 <abril.sar03@gmail.com>
2025-11-08 08:24:06 -0400

    feat(api): :sparkles: create auth dto

---
+++ src/main/java/com/ecommerce/api/dto/AuthResponse.java
@@ -0,0 +1,23 @@
+package com.ecommerce.api.dto;
+
+public class AuthResponse {
+    private String accessToken;
+    private long expiresIn;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
+
