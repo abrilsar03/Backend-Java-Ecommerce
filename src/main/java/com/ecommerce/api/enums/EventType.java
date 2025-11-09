8c5aa7235ed1bc6ad7cd4bb011bdf54c4354c7ba
abrilsar03 <abril.sar03@gmail.com>
2025-11-09 19:32:27 -0400

    feat: :sparkles: add log migration definition

---
+++ src/main/java/com/ecommerce/api/enums/EventType.java
@@ -0,0 +1,5 @@
+package com.ecommerce.api.enums;
+
+public enum EventType {
+    TOKEN_CREATED, ORDER_PLACED, PAYMENT_ATTEMPT
+}
---
+++ src/main/java/com/ecommerce/api/enums/LogLevelType.java
@@ -0,0 +1,5 @@
+package com.ecommerce.api.enums;
+
+public enum LogLevelType {
+    DEBUG, INFO, WARN, ERROR
+}
